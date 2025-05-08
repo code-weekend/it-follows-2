cb60660f3b70c50d07da811cd3fba0ab62632beb
marco-souza <marco-souza@users.noreply.github.com>
2025-05-08 00:06:15 +0000

    deploy: b43a26057fadf8a5b3b591dcc7e61335b4a2b21c

--- game.js
+++ game.js
@@ -284,6 +284,6 @@ Module.expectedDataFileDownloads++;
     }
 
   }
-  loadPackage({"package_uuid":"680ce949-cb49-4012-9b28-fd90d67928ca","remote_package_size":9193,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":9193,"audio":false}]});
+  loadPackage({"package_uuid":"9d9ef9ea-472a-4931-aa59-4d1336a4b6a6","remote_package_size":9194,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":9194,"audio":false}]});
 
 })();
