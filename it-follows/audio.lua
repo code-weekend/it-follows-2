6a09c79f4daf423e3e93c5213a094f559c706754
Alexsander Souza <92753962+parvilucifera@users.noreply.github.com>
2025-05-19 08:55:22 -0300

    init audio engine

---
+++ it-follows/audio.lua
@@ -0,0 +1,28 @@
+--- Audio Module
+--- Handles BGM, sound effects and volume parametes
+local Audio = {}
+Audio.__index = Audio
+
+function Audio:new()
+  local instance = setmetatable({}, Audio)
+  local success, bgm = pcall(love.audio.newSource, 'assets/audio/bgm.mp3', 'stream')
+    if not success then
+        print("Erro ao carregar bgm: " .. tostring(bgm))
+    end
+        instance.bgm = bgm
+  -- TODO: add sound effect sources
+  bgm:play()
+  return instance
+end
+
+function Audio:bgmUpdate(status)
+  if status == "started" then
+    self.bgm:play()
+    return
+  end
+  self.bgm:stop()
+  -- TODO: handle change bgm at certain score milestones
+  -- TODO: compose original music =D
+end
+
+return Audio
